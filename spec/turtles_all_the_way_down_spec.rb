0d31876f3070d3d1612b6eb48eca34330d63ce3c
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-12 20:17:47 +0000

    Automatically backed up by Learn

--- spec/turtles_all_the_way_down_spec.rb
+++ spec/turtles_all_the_way_down_spec.rb
@@ -4,5 +4,4 @@ describe "turtles_all_the_way_down" do
   it "`turtle_traits` should be able to iterate through the array without breaking." do
     expect(turtle_traits(turtles)).to eq([["serious", "leader"], ["party dude"], ["cool", "rude"], ["loves being a turtle"]])
   end
-  binding.pry
 end
